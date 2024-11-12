6ec045972474ecb4a98227817650c3db2ead6827
ankit1272223 <ankitomprakashtiwari2001@gmail.com>
2024-11-12 17:16:03 +0530

    update aborts

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -3,12 +3,8 @@ import React from 'react'
 function about() {
   return (
     <>
-<<<<<<< HEAD
       <h1 className='bg-blue-500'>about</h1>
       <h1 className='bg-blue-900'>new bug fix in abot</h1> 
-=======
-      <h1>about</h1>
->>>>>>> master
     </>
 
   )

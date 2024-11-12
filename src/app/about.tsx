06ba1faada9b55d68b8ac71d8def048e30b29fe4
ankit1272223 <ankitomprakashtiwari2001@gmail.com>
2024-11-12 16:54:55 +0530

    update about

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function about() {
   return (
     <>
-      <h1>about</h1>
-      <h1>new bug fix in abot</h1> 
+      <h1 className='bg-blue-500'>about</h1>
+      <h1 className='bg-blue-900'>new bug fix in abot</h1> 
     </>
 
   )

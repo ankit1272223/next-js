ccb654e6e7c17fb5a403e215cb19a2c32cfdc2f2
ankit1272223 <ankitomprakashtiwari2001@gmail.com>
2024-11-13 16:31:44 +0530

    about add div

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 function about() {
   return (
     <>
-      <h1 className='bg-blue-500'>about</h1>
-      <h1 className='bg-blue-900'>new bug fix in abot</h1>
-      <h1>ankit DOB 2003</h1>
+      <div className='bg-slate-400'>
+        <h1 className='bg-blue-500'>about</h1>
+        <h1 className='bg-blue-900'>new bug fix in abot</h1>
+        <h1>ankit DOB 2003</h1>
+      </div>
+
     </>
 
   )

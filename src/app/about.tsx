a69e1b7fc761ef417e20e4289dc95e83fe4b58f8
ankit1272223 <ankitomprakashtiwari2001@gmail.com>
2024-11-14 15:25:26 +0530

    update new h2 about

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -7,6 +7,7 @@ function about() {
         <h1 className='bg-blue-500'>about</h1>
         <h1 className='bg-blue-900'>new bug fix in abot</h1>
         <h1>ankit DOB 2003</h1>
+        <h2>ankit DOB 2003</h2>
       </div>
 
     </>
